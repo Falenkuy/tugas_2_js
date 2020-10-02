f15c89d562817d162283d81b10016e0086d9f291
Valencinus Aten <valenaten22@gmail.com>
2020-10-02 21:19:51 +0700

    Tugas 2 Js
    Tugas ini saya  kumpulkan ke tim Niomic

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var number = 12;
+var boolean = true ;
+var string = "nama";
+var null1  = null;
+var array = [12,13,"VALEN",14];
+
+console.log(number);
+console.log(boolean);
+console.log(string);
+console.log(null1 );
+console.log(array);
